a1ee554c253b55ea3065ca9e194923692c9a08cb
Matthias Ngo <m-ngo@web.de>
2022-01-12 17:48:41 +0100

    Added Youtube Cookie Clicker

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -1,29 +1,31 @@
-console.log("Found goog...");
+console.log("Found yout...");
 
 
 const docLanguage = document.getElementsByTagName('html')[0].getAttribute('lang');
 switch (docLanguage.slice(0, 2)) {
     case "de":
-        clickParentOfTagElementByNameAmountOfTimesTimes("div", "Anpassen", 1);
+        clickParentOfTagElementByNameAmountOfTimesTimes("yt-formatted-string", "ANPASSEN", 1);
         clickParentOfTagElementByNameAmountOfTimesTimes("span", "Aus", 3);
-        clickParentOfTagElementByNameAmountOfTimesTimes("span", "Bestätigen", 1);
+        clickParentOfTagElementByNameAmountOfTimesTimes("span", "BESTÄTIGEN", 5);
         break;
     case "en":
-        clickParentOfTagElementByNameAmountOfTimesTimes("div", "Customise", 1);
+        clickParentOfTagElementByNameAmountOfTimesTimes("yt-formatted-string", "CUSTOMIZE", 1);
         clickParentOfTagElementByNameAmountOfTimesTimes("span", "Off", 3);
-        clickParentOfTagElementByNameAmountOfTimesTimes("span", "Confirm", 1);
+        clickParentOfTagElementByNameAmountOfTimesTimes("span", "CONFIRM", 5); 
         break;   
 }
 
-console.log("...le");
+console.log("...ube");
+
 
 function clickParentOfTagElementByNameAmountOfTimesTimes(tagName, name, amountOfTimes) {
     const tagElements = document.getElementsByTagName(tagName);
-    var x = 0;
+    var counter = 0;
     for (const tagElement of tagElements) {
-        if (x == amountOfTimes) return;
+        console.log("Searching for Element")
+        if (counter == amountOfTimes) return;
         if (tagElement.innerText === name) {
-            x += 1;
+            counter += 1;
             tagElement.parentElement.click();
         }
     }
